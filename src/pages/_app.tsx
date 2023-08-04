17eb5c3a1749e155608794c41bf609144392499d
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-04 12:35:01 -0300

    refactor: move head for _document file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import { ApolloProvider } from '@apollo/client'
@@ -15,16 +14,6 @@ function App({ Component, pageProps }: AppProps) {
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <SubscriptionFormContextProvider>
-          <Head>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-
-            <meta name="description" content="Loren ipsum dolor" />
-
-            <title>Union</title>
-          </Head>
-
           <GlobalStyles />
 
           <Component {...pageProps} />
