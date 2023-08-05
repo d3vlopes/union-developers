ac9949cde304ac0262486bef5d22fb16661ff107
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-05 17:05:57 -0300

    chore: add wrapper for lib sentry

---
+++ src/libs/monitoring/sentry/helpers/index.ts
@@ -0,0 +1,26 @@
+import { Sentry } from '..'
+
+interface CaptureMessageArgs extends Sentry.Breadcrumb {
+  user?: Sentry.User
+  error?: unknown
+  logMessage?: string
+}
+
+export function captureMessage({
+  category,
+  message,
+  level,
+  user,
+  error,
+  logMessage,
+}: CaptureMessageArgs) {
+  Sentry.addBreadcrumb({
+    category,
+    message,
+    level,
+  })
+
+  if (user) Sentry.setUser(user)
+  if (logMessage) Sentry.captureMessage(logMessage)
+  if (error) Sentry.captureException(error)
+}
