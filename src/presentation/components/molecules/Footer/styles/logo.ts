1e634bdad2278e3c1fe16a4b87eab682f0f2d160
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-08 20:43:20 -0300

    fix: remove logo size of Footer

--- src/presentation/components/molecules/Footer/styles/logo.ts
+++ src/presentation/components/molecules/Footer/styles/logo.ts
@@ -1,13 +1,3 @@
 import styled from 'styled-components'
 
-import { media } from '@/presentation/styles/helpers'
-
-export const Logo = styled.img`
-  width: 8.8rem;
-  height: 2.8rem;
-
-  ${media.md`
-    width: 11.5rem;
-    height: 3.6rem;
-  `}
-`
+export const Logo = styled.img``
