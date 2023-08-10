6f2cb685e2bf38a1f5b4bec8f9725b30296e17fb
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-10 13:12:28 -0300

    chore: remove webmanifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,7 +65,6 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
 
           <meta
             name="google-site-verification"
