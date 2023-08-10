747b53366ae1567722725e990bba516a80a789f2
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-10 13:05:55 -0300

    test: import mocks folder on test setup file

---
+++ __tests__/mocks/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  observe() {
+    return null
+  }
+
+  disconnect() {
+    return null
+  }
+
+  unobserve() {
+    return null
+  }
+} as any
