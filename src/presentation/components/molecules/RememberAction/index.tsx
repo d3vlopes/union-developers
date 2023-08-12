efbd3cad01b1091fbbfcdc9a00833a261c015bcf
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-08-12 15:00:03 -0300

    chore: wip

--- src/presentation/components/molecules/RememberAction/index.tsx
+++ src/presentation/components/molecules/RememberAction/index.tsx
@@ -1,14 +1,10 @@
 import { RememberActionModel } from '@/domain/models'
 
 import { handleAnalityClick } from '@/libs/monitoring'
-import { motion } from '@/libs/animations'
 
 import { Button } from '@/presentation/components/atoms'
 
-import {
-  makeFlashAnimation,
-  makeFloatingAnimation,
-} from './animations'
+import { makeFloatingAnimation } from './animations'
 
 import * as S from './styles'
 
@@ -23,7 +19,6 @@ export const RememberAction = ({
   logoAlt,
 }: RememberActionProps) => {
   const logoAnimation = makeFloatingAnimation()
-  const subtitleAnimation = makeFlashAnimation()
 
   function handleButtonClick() {
     handleAnalityClick({
@@ -38,7 +33,7 @@ export const RememberAction = ({
       <S.Logo src={logoUrl} alt={logoAlt} {...logoAnimation} />
       <S.ContentWrapper>
         <h2>{title}</h2>
-        <motion.h3 {...subtitleAnimation}>{subtitle}</motion.h3>
+        <h3>{subtitle}</h3>
         <a href={buttonTarget} onClick={handleButtonClick}>
           <Button>{buttonText}</Button>
         </a>
